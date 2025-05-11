f12c4fa476c017d29b57c8b7932a150e8f344df4
wwiecek <20645798+wwiecek@users.noreply.github.com>
2025-05-11 13:33:13 +0200

    further data processing prep

--- README.md
+++ README.md
@@ -1,8 +1,21 @@
 # Sources of data in this project
 
-Up-to-date document with short descriptions is [available as a Google Doc](https://docs.google.com/document/d/1ZZAEwfHS0aAELN1w1lqEO6-eeu31_WdL/edit?usp=sharing&ouid=114240127695432531696&rtpof=true&sd=true).
+Up-to-date document with short descriptions is 
+[available as a Google Doc](https://docs.google.com/document/d/1ZZAEwfHS0aAELN1w1lqEO6-eeu31_WdL/edit?usp=sharing&ouid=114240127695432531696&rtpof=true&sd=true).
 
+General methodology for creating sets of z-values:
 
+- Extract z as `b/se` or `-qnorm(pvalue/2)`
+- If some `z` values were truncated in some way, keep an indicator for that
+- Extract year: if available, year of intervention (Askarov et al), if not, year of study; in one case (Yang et al) I retrieve it from study titles, since half of them have dates available
+- Keep study indicator, labelled `studyid`
+- For sets of meta-analyses, also retain `metaid` indicator
+- Remove studies where z's are NA before saving (to reduce size of data)
 
+Post-processing:
 
-
+- Truncate very large z values ourselves 
+- Add number of observations in each study, 
+- Add number of studies in each meta-analysis 
+- For large datasets, choose one z-value per study
+- ...something about calculating weights
--- process_data.R
+++ process_data.R
@@ -16,17 +16,20 @@ dt_list <- list()
 # Brodeur et al ------
 
 dt_list[["Brodeur"]] <- read_dta("data/Brodeur/merged.dta") %>% 
-  rename(z = myz) %>% 
-  mutate(z = ifelse(abs(z) > 20, 20, z)) %>% 
-  mutate(trunc = ifelse(abs(z) == 20, "truncated", "not truncated")) %>% 
+  # rename(z = myz) %>% 
+  # mutate(z = ifelse(abs(z) > 20, 20, z)) %>% 
+  # mutate(trunc = ifelse(abs(z) == 20, "truncated", "not truncated")) %>% 
   filter(method=="RCT") %>%
-  mutate(
-    prereg = factor(prereg, labels=c("not pre-registered","pre-registered")), 
-    papregistry = factor(papregistry,labels=c("no analysis plan","analysis plan"))) %>% 
-  filter(!is.na(z)) %>% 
-  group_by(title) %>% 
-  mutate(k=n()) %>% 
-  ungroup()
+  # mutate(
+  # prereg = factor(prereg, labels=c("not pre-registered","pre-registered")), 
+  # papregistry = factor(papregistry,labels=c("no analysis plan","analysis plan"))) %>% 
+  transmute(
+    metaid = NA,
+    studyid = title,
+    z = myz,
+    b = NA,
+    se = NA,
+    year = year)
 
 
 
@@ -37,12 +40,19 @@ dt_list[["Brodeur"]] <- read_dta("data/Brodeur/merged.dta") %>%
 dt_list[["Askarov"]] <- read_dta("data/Askarov/Mandatory data-sharing 30 Aug 2022.dta") %>% 
   mutate(z = effectsize/standarderror) %>% 
   dplyr:: filter(Excludegroup==0) %>% 
-  # I'm guessing:
-  mutate(data_sharing = (EVENT==0 | INTERVENTION==1)) %>% 
-  filter(!is.na(z)) %>% 
-  group_by(studyid) %>% 
-  mutate(k=n()) %>% 
-  ungroup()
+  transmute(
+    metaid = NA,
+    studyid = studyid,
+    z = effectsize/standarderror,
+    b = effectsize,
+    se = standarderror,
+    year = YEARintervention)
+# I'm guessing:
+# mutate(data_sharing = (EVENT==0 | INTERVENTION==1)) %>% 
+# filter(!is.na(z)) %>% 
+# group_by(studyid) %>% 
+# mutate(k=n()) %>% 
+# ungroup()
 
 
 
@@ -57,14 +67,17 @@ dt_list[["Askarov"]] <- read_dta("data/Askarov/Mandatory data-sharing 30 Aug 202
 # meta-analytic articles"
 
 dt_list[["Arel-Bundock"]] <- read.csv2("data/ArelBundock/estimates.csv",sep=",") %>% 
-  mutate(z = as.numeric(estimate)/as.numeric(std.error)) %>% 
-  filter(!is.na(z)) %>% 
-  group_by(study_id) %>% 
-  mutate(k=n()) %>% 
-  ungroup() 
-
-# Joined and cleaned up list of datasets
-dt_clean <- dt_list %>% lapply(ungroup) %>% lapply(select, z, k) %>% bind_rows(.id = "metastudy")
+  transmute(
+    metaid = meta_id, 
+    studyid = study_id,
+    z = as.numeric(estimate)/as.numeric(std.error),
+    b = as.numeric(estimate),
+    se = as.numeric(std.error),
+    year = study_year) 
+# filter(!is.na(z)) %>% 
+# group_by(study_id) %>% 
+# mutate(k=n()) %>% 
+# ungroup() 
 
 
 
@@ -80,10 +93,21 @@ dt_list[["WWC"]] <- read_csv("data/WWC/Kraft_wwc_merge.csv") %>%
          b = as.numeric(Effect.size)) %>% 
   mutate(sign = sign(b),
          z = sign(b)*qnorm(1 - pval/2)) %>% 
-  dplyr::filter(!is.na(z)) %>% 
-  group_by(cite_trunc) %>% 
-  mutate(k=n()) %>% 
-  ungroup()
+  transmute(
+    metaid = NA, 
+    studyid = cite_trunc,
+    z = sign(b)*qnorm(1 - pval/2),
+    b = b,
+    se = NA,
+    year = Publication.year) 
+
+# potentially also of interest:
+# read_csv("data/WWC/Kraft_wwc_merge.csv") %>% pull(Academic.subject) %>% table
+
+  # dplyr::filter(!is.na(z)) %>% 
+  # group_by(cite_trunc) %>% 
+  # mutate(k=n()) %>% 
+  # ungroup()
 
 
 
@@ -92,11 +116,30 @@ dt_list[["WWC"]] <- read_csv("data/WWC/Kraft_wwc_merge.csv") %>%
 # Datafile dat_processed_rob.csv may be downloaded from 
 # https://github.com/Yefeng0920/replication_EcoEvo_git/blob/main/data/sensitivity/dat_processed_rob.csv
 
+extract_year_yang <- function(x) {
+  # Find the last 4 consecutive digits in the string
+  year_match <- regexpr("\\d{4}[^0-9]*$", x)
+  if (year_match > 0) {
+    potential_year <- as.numeric(substr(x, year_match, year_match + 3))
+    if (potential_year >= 1950 && potential_year <= 2025) {
+      return(potential_year)
+    }
+  }
+  return(NA)
+}
 
 dt_list[["Yang et al"]] <- read.csv("data/Yang/dat_processed_rob.csv") %>% 
-  filter(!is.na(z)) %>%
-  group_by(study) %>% 
-  mutate(k=n())
+  transmute(
+    metaid = NA, 
+    studyid = study,
+    z = z,
+    b = es,
+    se = se) %>%
+  mutate(year = sapply(studyid, extract_year_yang))
+  # mutate(year = as.numeric(sub(".*?(\\d{4}).*", "\\1", studyid))) %>% summary
+  # filter(!is.na(z)) %>%
+  # group_by(study) %>% 
+  # mutate(k=n())
 
 
 # Costello and Fox -----
@@ -107,13 +150,21 @@ dt_list[["Yang et al"]] <- read.csv("data/Yang/dat_processed_rob.csv") %>%
 
 dt_list[["Costello and Fox"]] <- 
   read.csv("data/Yang/main_dat_processed.csv") %>% 
-  # pull(meta.analysis.id) %>% n_distinct()
-  filter(!is.na(z)) %>% 
-  group_by(key) %>% 
-  # Erik chose one 
-  slice_sample(n = 1) %>% 
-  group_by(study2) %>% 
-  mutate(k = n())
+  transmute(
+    metaid = meta.analysis.paper, 
+    studyid = study2,
+    z = z,
+    b = eff.size,
+    se = se.eff.size,
+    year = study.year) 
+
+  # # pull(meta.analysis.id) %>% n_distinct()
+  # filter(!is.na(z)) %>% 
+  # # group_by(key) %>% 
+  # # # Erik chose one 
+  # # slice_sample(n = 1) %>% 
+  # group_by(study2) %>% 
+  # mutate(k = n())
 
 
 
@@ -125,11 +176,17 @@ dt_list[["Jager and Leek"]] <-
   pvalueData %>% 
   data.frame() %>% 
   mutate_at(c('pvalue','year'), as.numeric) %>%
-  mutate(z=-qnorm(pvalue/2)) %>% 
-  mutate(truncated = factor(pvalueTruncated,labels=c("not truncated","truncated"))) %>% 
-  group_by(title) %>%  
-  mutate(k=n()) %>% 
-  ungroup()
+  transmute(
+    metaid = NA,
+    studyid = title,
+    z=-qnorm(pvalue/2),
+    b = NA, se = NA,
+    truncated = factor(pvalueTruncated,labels=c("not truncated","truncated")),
+    year = year) 
+  # group_by(title) %>%  
+  # mutate(k=n()) %>% 
+  # ungroup()
+
 
 
 # Sladekova -----
@@ -138,21 +195,27 @@ dfs <- lapply(list.files("data/Sladekova/", full.names = TRUE), read.csv)
 names(dfs) <- list.files("data/Sladekova/")
 
 dt_list[["Sladekova"]] <- lapply(dfs, function(df){
-  df = na.omit(df[,c("yi", "vi")])
-  df = data.frame(
-    b = 0.5*log((1+df$yi)/(1 - df$yi)),
-    se = sqrt(df$vi)
-  )
-  df = df[!is.na(df$b) & !is.infinite(df$b),]
-  return(df)
+  df %>% 
+    filter(!is.na(yi) & !is.na(vi)) %>% 
+    transmute(
+      b = 0.5*log((1 + yi)/(1 - yi)),
+      se = sqrt(vi)) 
+      # studyid = author, 
+      # year = year)
 }) %>% 
   bind_rows(.id = "metaid") %>% 
-  mutate(z = b/se) %>% 
-  filter(!is.na(metaid)) %>%
-  filter(!is.na(z)) %>%
-  group_by(metaid) %>% 
-  mutate(k=n()) %>% 
-  ungroup() 
+  filter(!is.na(b) & !is.infinite(b)) %>% 
+  mutate(z = b/se,
+         year = NA,
+         studyid = 1:nrow(.)) %>% 
+  filter(!is.na(metaid)) 
+  # filter(!is.na(z)) %>%
+  # group_by(metaid) %>% 
+  # mutate(k=n()) %>% 
+  # ungroup() 
+
+# year and studyid could be extracted, but they would need some cleaning up of strings
+# in each of the 406 files, so it may be best to do it later
 
 
 
@@ -160,18 +223,23 @@ dt_list[["Sladekova"]] <- lapply(dfs, function(df){
 
 dt_list[["metaPsy"]] <- readRDS("data/metaPsy/metapsy_dt.rds") %>% 
   lapply(function(df) {
-    if (all(c("study", ".g", ".g_se") %in% colnames(df))) {
-      return(df[, c("study", ".g", ".g_se"), drop = FALSE])
+    if (all(c("study", "year", ".g", ".g_se") %in% colnames(df))) {
+      return(df[, c("study", "year", ".g", ".g_se"), drop = FALSE])
     } else {
       return(data.frame()) 
     }
   }) %>% 
-  bind_rows(.id = "set") %>% 
-  mutate(z = .g/.g_se) %>% 
-  filter(!is.na(z)) %>%
-  group_by(set, study) %>% 
-  mutate(k=n()) %>% 
-  ungroup() 
+  bind_rows(.id = "metaid") %>% 
+  transmute(
+    studyid = study,
+    year = year,
+    z = .g/.g_se,
+    b= .g,
+    se = .g_se)
+  # filter(!is.na(z)) %>%
+  # group_by(set, study) %>% 
+  # mutate(k=n()) %>% 
+  # ungroup() 
 
 
 
@@ -181,16 +249,43 @@ load("data/BarnettWren/Georgescu.Wren.RData")
 
 dt_list[["BarnettWren"]] <- complete %>% 
   filter(!mistake) %>% 
+  # 0.3% of available values have CI widths other than 95%, let's remove these
   mutate(se = (log(upper) - log(lower))/(2*1.96)) %>% 
   mutate(b = (log(upper) + log(lower))/2) %>% 
   mutate(z = b/se) %>% 
-  filter(!is.na(z)) %>% 
-  # There is median 2 values per paper (max = 5,045), Erik sampled one from each
-  # (dplyr syntax can be a tiny bit slow here, unfortunately)
-  group_by(pubmed) %>%
-  slice(sample(n(), 1)) %>%
-  ungroup()
+  transmute(studyid = pubmed,
+            z = b/se,
+            b = b,
+            se = se,
+            year = Year)
+# filter(!is.na(z))
+# There is median 2 values per paper (max = 5,045), Erik sampled one from each
+# (dplyr syntax can be a tiny bit slow here, unfortunately)
+# group_by(pubmed) %>%
+# slice(sample(n(), 1)) %>%
+# ungroup()
 
 
+# Cochrane -----
 
+load("data/Cochrane/CDSR.RData") #this has object 'data' in it, with >400,000 rows
+
+dt_list[["CDSR"]] <- data %>% 
+  filter(RCT=="yes") %>% 
+  filter(outcome.group=="efficacy" & outcome.nr==1) %>% 
+  transmute(
+    metaid = id,
+    studyid = study.name,
+    z = b/s,
+    b = b,
+    se = s,
+    year = study.year)
+
+# Save a list of all datasets for further processing -----
 saveRDS(dt_list, file = "data/dt_list.rds") #20-30 MBs of data
+
+lapply(dt_list, filter, !is.na(z))
+saveRDS("data/BEAR.rds")
+# Joined and cleaned up list of datasets
+dt_clean <- dt_list %>% lapply(ungroup) %>% lapply(select, z, k) %>% bind_rows(.id = "metastudy")
+

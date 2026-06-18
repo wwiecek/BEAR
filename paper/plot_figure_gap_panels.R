955b2482dc128d01813dc38f5880dc649ee27014
Witold Wiecek <20645798+wwiecek@users.noreply.github.com>
2026-06-18 14:47:27 +0100

    Update paper gap panel figure

--- paper/plot_figure_gap_panels.R
+++ paper/plot_figure_gap_panels.R
@@ -1,14 +1,19 @@
 # library(geomtextpath)
 library(tidyverse)
 library(patchwork)
-library(geomtextpath)
 
 source("R/settings.R")
+source("R/paper_selection.R")
 source("R/psr.R")
 source("R/helpers.R")
 source("R/mix.R")
 
-mfl <- load_all_mixtures(exclude = paper_do_not_include)
+set.seed(42)
+
+mfl <- load_all_mixtures(exclude = paper_analysis_exclude)
+plots_per_row <- 4
+panel_title_size <- 7.2
+a4_text_width_cm <- 16.5
 
 small_gap_plot <- function(fit, single = FALSE){
   z  <- seq(0,5,0.1)
@@ -42,7 +47,7 @@ small_gap_plot <- function(fit, single = FALSE){
     geom_abline(intercept=pow,slope=0,linetype="dashed",alpha=0.5) +
     geom_line() +
     geom_point(data = hit_df, size = 1) +
-    {if(single) geom_textline(aes(label = label),vjust=1.5,size=3.5)} +
+    {if(single) geom_text(aes(label = label), hjust = 1, size = 3.5)} +
     # scale_y_continuous(minor_breaks = seq(0,1,0.05), breaks = seq(0,1,0.1),lim=c(0,1)) +
     scale_y_continuous(breaks = seq(0,1,0.25),lim=c(0,1)) +
     scale_x_continuous(minor_breaks = seq(0,5,0.5),
@@ -59,32 +64,51 @@ small_gap_plot <- function(fit, single = FALSE){
 
 
 # Arranging all of the plots on a single page
-ds_tbl <- read_csv("paper/power_sign_rep.csv") %>% 
-  mutate(group = bear_dataset_classes$workflow_classification[dataset]) %>%
-  transmute(dataset, group, PoS = assurance)  %>% 
-  arrange(desc(PoS))
+psr_table <- read_csv("paper/power_sign_rep.csv", show_col_types = FALSE) %>%
+  transmute(dataset, PoS = assurance)
 
 mk_small_gap <- function(ds)
   small_gap_plot(mfl[[ds]]) +
   labs(title = bear_labels[ds]) +
-  theme(plot.title  = element_text(size = 8),
-        axis.text   = element_text(size = 7),
-        axis.title.x  = element_text(size = 7),
+  theme(plot.title  = element_text(size = panel_title_size),
+        axis.text   = element_text(size = panel_title_size * 0.75),
+        axis.title.x  = element_text(size = panel_title_size * 0.75),
         legend.position = "none")
 
-curmet <- ds_tbl %>% 
-  filter(group %in% c("curated", "meta")) %>% 
-  arrange(desc(PoS)) %>% pull(dataset)
-rep <- ds_tbl %>% filter(group == "replications")  %>% pull(dataset)
-scr <- ds_tbl %>% filter(group == "scrape")  %>% pull(dataset)
+category_row <- function(datasets) {
+  datasets <- order_paper_datasets(datasets, psr_table)
+  plots <- lapply(datasets, mk_small_gap)
+  n_spacers <- (plots_per_row - length(plots) %% plots_per_row) %% plots_per_row
+  c(plots, rep(list(plot_spacer()), n_spacers))
+}
 
-plotlist <- lapply(c(curmet, rep, scr), mk_small_gap)
-plotlist <- append(plotlist, list(plot_spacer()), after = 11)
-wrap_plots(plotlist, ncol = 4) +
+available_datasets <- intersect(names(mfl), paper_selected_datasets)
+paper_groups <- lapply(paper_dataset_groups, intersect, available_datasets)
+
+plotlist <- c(
+  category_row(paper_groups$curated_evidence),
+  category_row(paper_groups$domain_sample)
+)
+
+expected_group_sizes <- c(curated_evidence = 8L, domain_sample = 8L)
+actual_group_sizes <- c(curated_evidence = length(paper_groups$curated_evidence),
+                        domain_sample = length(paper_groups$domain_sample))
+if (!identical(actual_group_sizes, expected_group_sizes)) {
+  stop("Expected two collection groups of 8 datasets, but found: ",
+       paste(names(actual_group_sizes), actual_group_sizes,
+             sep = " = ", collapse = ", "))
+}
+
+rows <- length(plotlist) / plots_per_row
+
+gap_plot_all <- wrap_plots(plotlist, ncol = plots_per_row) +
   plot_annotation(caption = "|z|") &
   theme(
     plot.caption = element_text(hjust = 0.5),
-    axis.text    = element_text(size = 8 * 0.75)
+    axis.text = element_text(size = panel_title_size * 0.75)
   )
 
-ggsave("paper/figures/gap_plot_all.pdf", height = 17, width = 14, units = "cm")
+ggsave("paper/figures/gap_plot_all.pdf", gap_plot_all,
+       height = rows * 4 * a4_text_width_cm / 14,
+       width = a4_text_width_cm, units = "cm",
+       device = cairo_pdf)
